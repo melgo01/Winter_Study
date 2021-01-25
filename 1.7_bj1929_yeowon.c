ab1cbdd25d997117af5bd56a14725ce86edbf6bb
melgo01 <melgo129@gmail.com>
2021-01-25 22:57:00 +0900

    bj1929

---
+++ 1.7_bj1929_yeowon.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void) {
+	int n, p[203644] = { 3,5, };
+	printf("%d %d %d %d", p[0], p[1], p[2], p[3]);
+	/*
+	for (int i = 7; i <= 1000000; i++) {
+		if()
+	}
+
+	int m, n;
+	scanf("%d %d", &m, &n);
+	for (int i = m; i <= n; i++) {
+		for (int j = 2; j <= i; j++) {
+			if (i == j) {
+				printf("%d\n", i);
+			}
+			else if (i % j == 0) {
+				break;
+			}
+		}
+	}
+	*/
+}
